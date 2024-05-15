fc5111b791959f14348fa7d687eccc249f0c1477
Veron1405 <leonardo.verondias1405@gmail.com>
2024-05-15 16:05:44 -0300

    Initial commit

---
+++ Lista C#-3/Lista C#-3/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+class Program
+{
+    /*static void Main(string[] args)
+    {
+
+        Console.WriteLine("Digite a quantidade de sorvetes que deseja comprar:");
+        string input = Console.ReadLine();
+
+        int quantidade = int.Parse(input);
+
+        double precoUnitario;
+
+        if (quantidade <= 25)
+        {
+            precoUnitario = 1.50; 
+        }
+        else
+        {
+            precoUnitario = 1.25; 
+        }
+
+        double valorTotal = quantidade * precoUnitario;
+
+        Console.WriteLine("Quantidade de sorvetes: " + quantidade);
+        Console.WriteLine("Valor total a pagar: R$ " + valorTotal.ToString("0.00"));
+    }*/
+
+    /*class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número:");
+            string input = Console.ReadLine();
+
+            int numero = int.Parse(input);
+
+            if (numero > 10)
+            {
+                int resultado = numero + 10 * 2;
+                Console.WriteLine("O resultado é: " + resultado);
+            }
+            else
+            {
+                int resultado = numero + 9 * 3 - 8;
+                Console.WriteLine("O resultado é: " + resultado);
+            }
+        }
+    }*/
+
+/*class Program3
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número:");
+        string input = Console.ReadLine();
+
+        int numero = int.Parse(input);
+
+        if (numero > 0)
+        {
+            Console.WriteLine("O número é positivo.");
+        }
+        else if (numero < 0)
+        {
+            Console.WriteLine("O número é negativo.");
+        }
+        else
+        {
+            Console.WriteLine("O número é zero.");
+        }
+    }
+}*/
+   
+
+/*class Program4
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o primeiro número:");
+        string input1 = Console.ReadLine();
+        int numero1 = int.Parse(input1);
+
+        Console.WriteLine("Digite o segundo número:");
+        string input2 = Console.ReadLine();
+        int numero2 = int.Parse(input2);
+
+        if (numero1 > numero2)
+        {
+            Console.WriteLine("O primeiro número é maior que o segundo.");
+        }
+        else if (numero1 < numero2)
+        {
+            Console.WriteLine("O segundo número é maior que o primeiro.");
+        }
+        else
+        {
+            Console.WriteLine("Os números são iguais.");
+        }
+    }
+}*/
+
+/*class Program5
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a primeira nota:");
+        string input1 = Console.ReadLine();
+        double nota1 = double.Parse(input1);
+
+        Console.WriteLine("Digite a segunda nota:");
+        string input2 = Console.ReadLine();
+        double nota2 = double.Parse(input2);
+  
+        double media = (nota1 + nota2) / 2;
+
+        if (media >= 7.0)
+        {
+            Console.WriteLine("Aprovado");
+        }
+        else
+        {
+            Console.WriteLine("Reprovado");
+        }
+    }
+}*/
+
+}
